ddfe8cefa3da9fdc167820bec15267ad332d4e3d
Katerina Kurochkina <katifedr@mail.ru>
2025-06-29 14:00:01 +0500

    users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import email_verification
-
+from .views import email_verification, UserListView, UserCreateView, DeleteAccountView, logout_view
+from django.contrib.auth.views import LoginView
 app_name = 'users'
 
 urlpatterns = [
-    # path('register/', UserCreateView.as_view(template_name="register.html"), name='register'),
-    # path('login/', LoginView.as_view(template_name="login.html"), name='login'),
-    # path('logout/', logout_view, name='logout'),
-    # path('delete/', DeleteAccountView.as_view(template_name="user_confirm_delete.html"), name='delete'),
+    path('', UserListView.as_view(), name='home'),
+    path('register/', UserCreateView.as_view(template_name="register.html"), name='register'),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('delete/', DeleteAccountView.as_view(template_name="user_confirm_delete.html"), name='delete'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
-    # path('', UserListView.as_view(), name='home')
 ]
